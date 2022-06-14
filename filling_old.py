2f3284067827ad412d6f4691c52ff15dcdb47757
dlcgold <dlcgold96@gmail.com>
2022-06-14 22:38:10 +0200

    robe

--- filling_old.py
+++ filling_old.py
@@ -1,6 +1,5 @@
 import math
-from gensim.parsing.preprocessing import remove_stopwords
-import WordCloud as WordCloud
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -529,47 +528,6 @@ def main():
             if key in bar_data.keys() and bar_data[key] == 0:
                 bar_data.pop(key)
     bacts = bacts_db_path_without_drugs
-
-    word_cloud_name = ""
-    word_cloud_des = ""
-    word_cloud_pap = ""
-    bad_words = ["infection", "Infection", "human", "Human", "pathogen", "Pathogen",
-                 "Bacterium", "bacterium", "Disease", "disease", "Clinical", "clinical"]
-    for bact_tmp in bacts:
-        name = bact_tmp.name
-        des = bact_tmp.description
-        pap = ""
-        for paper in bact_tmp.papers:
-            pap += paper.title
-        for bw in bad_words:
-            name = name.replace(bw, "")
-            des = des.replace(bw, "")
-            pap = pap.replace(bw, "")
-        word_cloud_name += (name + " ")
-        word_cloud_des += (des + " ")
-        word_cloud_pap += (pap + " ")
-
-    wordcloud = WordCloud(max_font_size=40, background_color="white", contour_color='#5e81ac',
-                          contour_width=0.1, scale=2).generate(remove_stopwords(word_cloud_name))
-    plt.figure()
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.savefig("plot_print/name_wc.png", dpi=500)
-    plt.close()
-
-    wordcloud = WordCloud(max_font_size=40, background_color="white", contour_color='#5e81ac',
-                          contour_width=0.1, scale=2).generate(remove_stopwords(word_cloud_des))
-    plt.figure()
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.savefig("plot_print/des_wc.png", dpi=500)
-
-    wordcloud = WordCloud(max_font_size=40, background_color="white", contour_color='#5e81ac',
-                          contour_width=0.1, scale=2).generate(remove_stopwords(word_cloud_pap))
-    plt.figure()
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.savefig("plot_print/pap_wc.png", dpi=500)
 
 
 if __name__ == "__main__":
